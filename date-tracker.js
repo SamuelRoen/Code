19e1d4dea1976f7334f8844032ea1dad8e599f3a
SamuelRoen <samuelroen@gmail.com>
2023-03-23 17:12:10 +0100

    date of the trip

---
+++ date-tracker.js
@@ -0,0 +1,35 @@
+function dateFunc() {
+        const date = new Date();
+        let day=date.getDate();
+        let month=date.getMonth();
+        let year=date.getFullYear();
+        let currentDate=day + "/" + month + "/" + year;
+        document.getElementById('date').innerHTML= "Today is: " + currentDate;
+}
+
+function remainingFunc(){
+        // create a new date object
+var myDate = new Date();
+
+// set the specific date to subtract
+var specificDate = new Date('June 5, 2023');
+
+// get the time value in milliseconds for both dates
+var myDateTime = myDate.getTime();
+var specificDateTime = specificDate.getTime();
+
+// calculate the difference in milliseconds
+var diffInMs = specificDateTime - myDateTime;
+
+// convert milliseconds to days
+var diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+
+// output the difference in days
+console.log(diffInDays);
+
+
+
+
+        document.getElementById('remain').innerHTML=
+        diffInDays + " days to your trip" + "<br>";
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('new').innerHTML='This text will apper in h1';
+var carName = "Volvo";
+var carName="WV";
+document.getElementById('car').innerHTML=carName;
+window.alert('Whtas up?')
+<button onclick="print()">Print this page!</button>
